d475fe9797bb0f4325f6e801a50483fa1c9e642f
Banura Randika <randika.banura@gmail.com>
2022-04-12 11:59:00 +0530

    Service methods add

--- lib/bindup/bindup.rb
+++ lib/bindup/bindup.rb
@@ -1,8 +1,9 @@
 require_relative "version"
+require_relative "service_methods"
 require_relative "service_creator"
 require_relative "configuration"
 require "yaml"
-require 'active_support/all'
+require "active_support/all"
 require "faraday"
 
 module Bindup
