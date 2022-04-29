a630998d39fde913c4e95d7e4eb18dc41f27a556
Banura Randika <randika.banura@gmail.com>
2022-04-29 11:59:00 +0530

    Rudocop formatted the bindup file

--- lib/bindup.rb
+++ lib/bindup.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "bindup/version"
-require_relative 'bindup/service_creator'
-require_relative 'bindup/configuration'
-require 'yaml'
+require_relative "bindup/service_creator"
+require_relative "bindup/configuration"
+require "yaml"
 
 # Bindup is customizable API wrapper
 #
