dd3a63013844379e8680b7056a3c554983f030a0
Banura Randika <randika.banura@gmail.com>
2022-05-01 11:59:00 +0530

    Configuration file add

--- lib/bindup.rb
+++ lib/bindup.rb
@@ -2,6 +2,7 @@
 
 require_relative "bindup/version"
 require_relative 'bindup/service_creator'
+require_relative 'bindup/configuration'
 require 'yaml'
 
 # Bindup is customizable API wrapper
@@ -12,29 +13,5 @@ require 'yaml'
 # setup multiple services at once so that integrating whole new service
 # is easy as changing the config
 module Bindup
-
-  class << self
-    attr_accessor :configuration
-    attr_reader :component_setup
-  end
-
-  def self.configure
-    self.configuration ||= Configuration.new
-    yield(configuration)
-
-    @component_setup = YAML.load_file(configuration.config_path)
-
-    Bindup::ServiceCreator.execute
-  end
-
-  class Configuration
-    attr_accessor :config_path, :log_response, :log_response_params
-
-    def initialize
-      @config_path = "config/initializer/bindup.yml"
-      @log_response = true
-      # { headers: true, bodies: true }
-      @log_response_params = nil
-    end
-  end
+  extend Configuration
 end
