7314918c4fb68729ef65063eb69ccd8cbe55e766
Banura Randika <randika.banura@gmail.com>
2022-05-02 11:59:00 +0530

    module comment add to main Bindup module

--- lib/bindup.rb
+++ lib/bindup.rb
@@ -4,8 +4,15 @@ require_relative "bindup/version"
 require_relative 'bindup/service_creator'
 require 'yaml'
 
-
+# Bindup is customizable API wrapper
+#
+# It is able to wrap API from other services so the Ruby application
+# use those API with out having to implement a new module to deal with
+# every single API or having to create a gem. Bindup will be able to
+# setup multiple services at once so that integrating whole new service
+# is easy as changing the config
 module Bindup
+
   class << self
     attr_accessor :configuration
     attr_reader :component_setup
