bfe35d9e3bb1fc0ee6711af41971ced2e2faf8ae
Banura Randika <randika.banura@gmail.com>
2022-06-08 11:59:00 +0530

    Test update add

--- spec/http_methods/http_put_spec.rb
+++ spec/http_methods/http_put_spec.rb
@@ -30,4 +30,22 @@ RSpec.describe "HTTP::PUT" do
     expect(JSON.parse(response_body)["form"]).to eq({})
     expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/x-www-form-urlencoded")
   end
+
+  it "does calls a PUT json API endpoint with extra parameters" do
+    params = { test: "test" }
+    response_body, = Bindup::BSSMW::V1.fourth_test_api(params, extra_params: params)
+
+    expect(JSON.parse(JSON.parse(response_body)["data"])).to eq(params.stringify_keys)
+    expect(JSON.parse(response_body)["args"]).to eq(params.stringify_keys)
+    expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/json")
+  end
+
+  it "does calls a PUT urlencoded API endpoint with extra parameters" do
+    params = { test: "test" }
+    response_body, = Bindup::Telco::V2.first_test_api(params, extra_params: params)
+
+    expect(JSON.parse(response_body)["form"]).to eq(params.stringify_keys)
+    expect(JSON.parse(response_body)["args"]).to eq(params.stringify_keys)
+    expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/x-www-form-urlencoded")
+  end
 end
