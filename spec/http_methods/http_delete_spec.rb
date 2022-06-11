945e7fa7ae10e328e025a976e177e48ed987211a
Banura Randika <randika.banura@gmail.com>
2022-06-11 11:59:00 +0530

    Test update add

--- spec/http_methods/http_delete_spec.rb
+++ spec/http_methods/http_delete_spec.rb
@@ -34,4 +34,22 @@ RSpec.describe "HTTP::DELETE" do
     expect(JSON.parse(JSON.parse(response_body)["data"])).to eq(params.stringify_keys)
     expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/json")
   end
+
+  it "does calls a DELETE urlencoded API endpoint with multiple extra parameters" do
+    params = { test: "test", "test-2": "test-2" }
+    response_body, = Bindup::Telco::V2.second_test_api(params, extra_params: params)
+
+    expect(JSON.parse(response_body)["args"]).to eq(params.stringify_keys)
+    expect(JSON.parse(response_body)["form"]).to eq(params.stringify_keys)
+    expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/x-www-form-urlencoded")
+  end
+
+  it "does calls a DELETE json API endpoint with extra parameters" do
+    params = { test: "test", "test-2": "test-2" }
+    response_body, = Bindup::Telco::V2.third_test_api(params, extra_params: params)
+
+    expect(JSON.parse(response_body)["args"]).to eq(params.stringify_keys)
+    expect(JSON.parse(JSON.parse(response_body)["data"])).to eq(params.stringify_keys)
+    expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/json")
+  end
 end
