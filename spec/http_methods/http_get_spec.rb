1ef9e25fa36fe12ba6d8243d9b5c9f5e33a2d839
Banura Randika <randika.banura@gmail.com>
2022-06-13 11:59:00 +0530

    Test update add

--- spec/http_methods/http_get_spec.rb
+++ spec/http_methods/http_get_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "HTTP::GET" do
   it "does calls a GET API endpoint" do
     response_body, = Bindup::BSSMW::V1.first_test_api
 
-    expect(JSON.parse(response_body)["args"]).to eq(nil)
+    expect(JSON.parse(response_body)["args"]).to eq({})
     expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/json")
   end
 
@@ -14,5 +14,21 @@ RSpec.describe "HTTP::GET" do
 
     expect(JSON.parse(response_body)["args"]).to eq(params.stringify_keys)
     expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/x-www-form-urlencoded")
+  end
+
+  it "does calls a GET urlencoded API endpoint with form payload" do
+    params = { test: "test" }
+    response_body, = Bindup::BSSMW::V1.fifth_test_api(params, extra_params: params)
+
+    expect(JSON.parse(response_body)["args"]).to eq(params.stringify_keys)
+    expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/x-www-form-urlencoded")
+  end
+
+  it "does calls a GET json API endpoint with json payload" do
+    params = { test: "test" }
+    response_body, = Bindup::BSSMW::V1.first_test_api(params, extra_params: params)
+
+    expect(JSON.parse(response_body)["args"]).to eq(params.stringify_keys)
+    expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/json")
   end
 end
