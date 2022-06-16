4349f7a4a15163105b0e0eed2d43a767928f5c8f
Banura Randika <randika.banura@gmail.com>
2022-06-16 11:59:00 +0530

    Test update add

--- spec/http_methods/http_get_spec.rb
+++ spec/http_methods/http_get_spec.rb
@@ -4,7 +4,15 @@ RSpec.describe "HTTP::GET" do
   it "does calls a GET API endpoint" do
     response_body, = Bindup::BSSMW::V1.first_test_api
 
-    expect(JSON.parse(response_body)["data"]).to eq(nil)
+    expect(JSON.parse(response_body)["args"]).to eq(nil)
     expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/json")
+  end
+
+  it "does calls a GET urlencoded API endpoint" do
+    params = { test: "test" }
+    response_body, = Bindup::BSSMW::V1.fifth_test_api(params)
+
+    expect(JSON.parse(response_body)["args"]).to eq(params.stringify_keys)
+    expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/x-www-form-urlencoded")
   end
 end
