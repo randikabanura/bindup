e903d78b457dd9b322067782385d2eca24329685
Banura Randika <randika.banura@gmail.com>
2022-06-18 11:59:00 +0530

    Test update add

--- spec/http_methods/http_put_spec.rb
+++ spec/http_methods/http_put_spec.rb
@@ -9,11 +9,25 @@ RSpec.describe "HTTP::PUT" do
     expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/json")
   end
 
+  it "does calls a PUT json API endpoint without parameters" do
+    response_body, = Bindup::BSSMW::V1.fourth_test_api
+
+    expect(JSON.parse(response_body)["data"]).to eq("")
+    expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/json")
+  end
+
   it "does calls a PUT urlencoded API endpoint" do
     params = { test: "test" }
     response_body, = Bindup::Telco::V2.first_test_api(params)
 
     expect(JSON.parse(response_body)["form"]).to eq(params.stringify_keys)
+    expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/x-www-form-urlencoded")
+  end
+
+  it "does calls a PUT urlencoded API endpoint without parameters" do
+    response_body, = Bindup::Telco::V2.first_test_api
+
+    expect(JSON.parse(response_body)["form"]).to eq({})
     expect(JSON.parse(response_body)["headers"]["Content-Type"]).to eq("application/x-www-form-urlencoded")
   end
 end
